547531cecddfa7a73aa167bb96688c0df8765448
Paul Holmes <pwholmes151@gmail.com>
2024-10-11 15:48:35 -0400

    rename test scripts

---
+++ backend/bin/test-apis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Make sure to call:
+#   export ACCESS_TOKEN="tokendata"
+# ...before using this test.  The token can be obtained using the test-login.sh script.  Tokens are (currently) valid for 1 day
+curl -d '{"username": "testuser", "password": "Test*123" }' -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -X GET http://localhost:5000/ingredients
