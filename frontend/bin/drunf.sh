e5e8bffeca3cf157673f2730be89695ffb6c3747
Paul Holmes <pwholmes151@gmail.com>
2024-10-24 21:42:02 -0400

    remove unused env variable from Docker run command

--- frontend/bin/drunf.sh
+++ frontend/bin/drunf.sh
@@ -3,5 +3,4 @@ sudo docker rm trackeats-frontend
 sudo docker run -d \
     --name trackeats-frontend \
     -p 80:8080 \
-    -e REACT_APP_BACKEND_BASE_URL=$REACT_APP_BACKEND_BASE_URL \
     lastcallsoftware/trackeats-frontend
