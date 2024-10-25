6201ec27973df6842307530cf0a5ebccfe635a30
Paul Holmes <pwholmes151@gmail.com>
2024-10-25 17:27:33 -0400

    add support for ssl

--- frontend/bin/drunf.sh
+++ frontend/bin/drunf.sh
@@ -3,4 +3,9 @@ sudo docker rm trackeats-frontend
 sudo docker run -d \
     --name trackeats-frontend \
     -p 80:8080 \
+    -p 443:8443 \
+    -p 5443:5443 \
+    -v /etc/ssl/certs:/etc/ssl/certs \
+    --add-host=host.docker.internal:host-gateway \
     lastcallsoftware/trackeats-frontend
+    
