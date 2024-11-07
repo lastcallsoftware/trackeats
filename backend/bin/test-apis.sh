3171de9402ffacf54ff0749f8592ac3f3a2dfdf5
Paul Holmes <pwholmes151@gmail.com>
2024-11-06 19:37:10 -0500

    remove unnecessarey parameters

--- backend/bin/test-apis.sh
+++ backend/bin/test-apis.sh
@@ -2,4 +2,4 @@
 # Make sure to call:
 #   export ACCESS_TOKEN="tokendata"
 # ...before using this test.  The token can be obtained using the test-login.sh script.  Tokens are (currently) valid for 1 day
-curl -d '{"username": "testuser", "password": "Test*123" }' -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS_TOKEN" -X GET http://localhost:5000/ingredient
+curl -H "Authorization: Bearer $ACCESS_TOKEN" -X GET http://localhost:5000/ingredient
