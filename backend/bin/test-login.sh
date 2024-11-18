fc86485f3d207697969016a14a5bf1b6480b5deb
Paul Holmes <pwholmes151@gmail.com>
2024-11-18 07:28:32 -0500

    parse out and export ACCESS_TOKEN so we don't have to do it manually all the time

--- backend/bin/test-login.sh
+++ backend/bin/test-login.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-curl \
+OUTPUT=`curl --no-progress-meter \
 -H "Content-Type: application/json" \
 -d '{"username": "testuser", "password": "Test*123" }' \
-http://localhost:5000/login
+http://localhost:5000/login`
+
+ACCESS_TOKEN=`echo $OUTPUT | grep -o '"access_token":"[^"]*' | grep -o '[^"]*$'`
+export ACCESS_TOKEN=$ACCESS_TOKEN
+echo "Logged in with ACCESS_TOKEN: " $ACCESS_TOKEN
