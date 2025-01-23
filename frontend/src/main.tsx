9ff92d73737938c1f814e03df21b3326372a0d40
Paul Holmes <pwholmes151@gmail.com>
2025-01-23 15:31:09 -0500

    rename IngredientProvider to DataProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter } from 'react-router-dom'
 import Nav from './components/Nav'
-import { IngredientProvider } from "./components/IngredientProvider"
+import { DataProvider } from "./components/DataProvider"
 import './index.css'
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <IngredientProvider>
+      <DataProvider>
         <Nav/>
-      </IngredientProvider>
+      </DataProvider>
     </BrowserRouter>
   </StrictMode>,
 )
