ecb96e665c5498be398e396418e9fe686e42920f
Paul Holmes <pwholmes151@gmail.com>
2025-02-14 02:20:08 -0500

    added missing tests

---
+++ backend/bin/test-get-recipe-ingredient.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Before using this script, make sure to export an auth token using:
+#   export ACCESS_TOKEN="tokendata"
+# A token can be obtained and exported automatically using:
+#   source ./bin/test-login.sh
+curl -s \
+-X GET \
+-H "Authorization: Bearer $ACCESS_TOKEN" \
+http://localhost:5000/recipe/2/recipe_ingredient/1 | jq
+#https://trackeats.lastcallsw.com:5443/recipe/1/recipe_ingredient/1
