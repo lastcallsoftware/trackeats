5d46aaa1cc7be0783db99318c918598125a657b3
Paul Holmes <pwholmes151@gmail.com>
2025-02-15 18:10:59 -0500

    finish first pass of Recipe Page

--- frontend/src/components/RecipesPage.tsx
+++ frontend/src/components/RecipesPage.tsx
@@ -1,45 +1,65 @@
 import { useContext, useState } from "react";
-import { DataContext } from "./DataProvider";
+import { DataContext, IRecipe } from "./DataProvider";
 import { IconContext } from "react-icons";
 import { MdAddCircleOutline, MdEdit, MdRemoveCircleOutline } from "react-icons/md";
 import RecipesTable from "./RecipesTable"
+import { useNavigate } from "react-router-dom";
 
 function MealsPage() {
-    const [, setSelectedMealRowId] = useState(null)
+    const navigate = useNavigate();
+    const [selectedRowId, setSelectedRowId] = useState(null)
     const context = useContext(DataContext)
     if (!context)
         throw Error("useDataContext can only be used inside a DataProvider")
+    const recipes = context.recipes
+    const deleteRecipe = context.deleteRecipe;
     const errorMessage = context.errorMessage;
 
-    const addRecipe = () => {
+    const addRecord = () => {
+        // Go to the edit form
+        navigate("/recipeForm");
     }
 
-    const editRecipe = () => {
+    const editRecord = () => {
+        if (selectedRowId) {
+            // Get the selected record and go to the edit form
+            const recipe = recipes.find((item:IRecipe) => item.id == selectedRowId);
+            navigate("/recipeForm", { state: { recipe } });
+        }
     }
 
-    const removeRecipe = () => {
+    const deleteRecord = () => {
+        // Get the selected record
+        if (selectedRowId) {
+            // Confirm the deletion request
+            const confirmed = confirm("Delete record.  Are you sure?  This cannot be undone.")
+            if (confirmed) {
+                // Delete the record from the database and the foods list.
+                deleteRecipe(selectedRowId);
+            }
+        }
     }
 
     return (
         <section className="recipePage">
             <section className="recipeTableContainer">
-                <RecipesTable setSelectedRowId={setSelectedMealRowId} />
+                <RecipesTable setSelectedRowId={setSelectedRowId} />
             </section>
 
             <section className="buttonBar">
-                <button className="addButton" onClick={addRecipe}>
+                <button className="addButton" onClick={addRecord}>
                     <IconContext.Provider value={{ size: "30px", color: "green"}}>
-                        <p className="editButtonText">Add</p><MdAddCircleOutline/>
+                        <p className="addButtonText">Add</p><MdAddCircleOutline/>
                     </IconContext.Provider>
                 </button>
-                <button className="editButton" onClick={editRecipe}>
+                <button className="editButton" onClick={editRecord}>
                     <IconContext.Provider value={{ size: "30px", color: "orange"}}>
                         <p className="editButtonText">Edit</p><MdEdit/>
                     </IconContext.Provider>
                 </button>
-                <button className="removeButton" onClick={removeRecipe}>
+                <button className="deleteButton" onClick={deleteRecord}>
                     <IconContext.Provider value={{ size: "30px", color: "red"}}>
-                        <p className="editButtonText">Delete</p><MdRemoveCircleOutline/>
+                        <p className="deleteButtonText">Delete</p><MdRemoveCircleOutline/>
                     </IconContext.Provider>
                 </button>
             </section>
