97fd49e6c47f572dae7f20e4e45f0cc77686eee4
Paul Holmes <pwholmes151@gmail.com>
2025-02-20 19:20:49 -0500

    disable pagination buttons when appropriate

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -2,13 +2,15 @@ import { Table } from "@tanstack/react-table"
 import { IFood, IRecipe } from "./DataProvider"
 import { MdKeyboardDoubleArrowLeft, MdKeyboardArrowLeft, MdKeyboardArrowRight, MdKeyboardDoubleArrowRight } from "react-icons/md";
 
+
 function Pagination({table}: {table:Table<IFood> | Table<IRecipe>}) {
     return(
         <section className="paginationButtons">
-            <button onClick={() => table.firstPage()}><MdKeyboardDoubleArrowLeft/></button>
-            <button onClick={() => table.previousPage()}><MdKeyboardArrowLeft/></button>
-            <button onClick={() => table.nextPage()}><MdKeyboardArrowRight/></button>
-            <button onClick={() => table.lastPage()}><MdKeyboardDoubleArrowRight/></button>
+            <button disabled={!table.getCanPreviousPage()} onClick={(e) => {e.preventDefault(); table.firstPage()}}><MdKeyboardDoubleArrowLeft/></button>
+            <button disabled={!table.getCanPreviousPage()} onClick={(e) => {e.preventDefault(); table.previousPage()}}><MdKeyboardArrowLeft/></button>
+            {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+            <button disabled={!table.getCanNextPage()} onClick={(e) => {e.preventDefault(); table.nextPage()}}><MdKeyboardArrowRight/></button>
+            <button disabled={!table.getCanNextPage()} onClick={(e) => {e.preventDefault(); table.lastPage()}}><MdKeyboardDoubleArrowRight/></button>
         </section>
     )
 }
