3f9d3eaa0c622c4be0b28fdc3a8857f926a9644a
Paul Holmes <pwholmes151@gmail.com>
2025-02-25 00:17:43 -0500

    create new script

---
+++ backend/bin/dlogin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Before you can execute any Docker command that access the Docker Hub,
+# You need to log on to it from the command line.
+# Of course, I'm not putting the password in this file since it's being
+# checked in to a public Git repository!  This script is mainly just to
+# remind myself what to do and how to do it.  The password is stored in
+# the usual place.
+docker login -u pwholmes
