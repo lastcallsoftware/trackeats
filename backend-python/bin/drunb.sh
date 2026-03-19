710dc8bc94bc80130a5a3ab8f874dc636b198000
Paul Holmes <pwholmes151@gmail.com>
2026-03-19 16:13:57 -0400

    get rid of this pointles environment variable

--- backend-python/bin/drunb.sh
+++ backend-python/bin/drunb.sh
@@ -3,5 +3,4 @@ sudo docker rm -f trackeats-backend >/dev/null 2>&1 || true
 sudo docker run -d \
     --name trackeats-backend \
     -p 5000:5000 \
-    -e ENV="DEV" \
     lastcallsoftware/trackeats-backend
