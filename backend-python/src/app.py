68154bd01a05786b2d4ee7c54e40b3492c29731f
Paul Holmes <pwholmes151@gmail.com>
2026-03-27 16:21:20 -0400

    Shufflearound some config so the db migrate command will work when run from in docker compose with minimal config.

--- backend-python/src/app.py
+++ backend-python/src/app.py
@@ -15,11 +15,13 @@ from crypto import Crypto
 from data import Data
 
 
-def create_app():
+def minimal_app_config() -> Flask:
+    """
+    This is the minimum app config needed for the database migration code to work.
+    """
     # STARTUP
     # -------
-    # Instantiate the Flask framework and register the "blueprints" (i.e., web
-    # services) defined in routes.py.
+    # Instantiate the Flask framework.
     app = Flask(__name__)
 
     # Configure the logger.  Any downstream modules will inherit this config.
@@ -50,37 +52,6 @@ def create_app():
     debug_mode = os.environ.get("FLASK_DEBUG")
     if debug_mode == "1":
         logging.info("Running in FLASK_DEBUG mode")
-
-    # BACKEND_BASE_URL is the name of THIS server.  We need it to build the links
-    # we put in confirmation emails.
-    hostname = os.environ.get("BACKEND_BASE_URL")
-    if (hostname is None):
-        logging.error("BACKEND_BASE_URL is missing -- exiting.")
-        exit(0)
-    logging.info("BACKEND_BASE_URL: " + hostname)
-
-
-    # INITIALIZE JWT LIB
-    # ------------------
-    # We're using a library (flask-jwt-extended) that handles JWT token management.
-    # It requires a key to be in the app context.  It should already be in the environment,
-    # just copy it over.
-    jwt_secret_key = os.environ.get("JWT_SECRET_KEY")
-    if not jwt_secret_key:
-        logging.error("JWT_SECRET_KEY is missing -- exiting.")
-        exit(0)
-    app.config['JWT_SECRET_KEY'] = jwt_secret_key
-    JWTManager(app)
-
-
-    # INITIALIZE CRYPTO LIB
-    # ---------------------
-    symmetric_key_b64 = os.environ.get("BACKEND_ENCRYPTION_KEY_B64")
-    if not symmetric_key_b64:
-        logging.error("BACKEND_ENCRYPTION_KEY_B64 is missing -- exiting.")
-        exit(0)
-    Crypto.initialize(symmetric_key_b64)
-
 
     # INITIALIZE DATABASE CONNECTION
     # ------------------------------
@@ -115,36 +86,84 @@ def create_app():
     # Instantiate the database connector.
     db.init_app(app)
 
-    # Instantiate the Flask migration object
+    # Instantiate the Flask migration object.  This thing is basically why I split the 
+    # app setup into two separate functions.  The "minimal" config is what's needed to
+    # run "flask db upgrade" from the command line.
     Migrate(app, db)
 
-    # Register blueprints
+    # Register the app's "blueprints" -- the REST endpoints defined in routes.py.
     app.register_blueprint(bp)
+
+    return app
+
+
+def additional_app_config(app: Flask):
+    """
+    This is additional config beyond the minimum, needed for the full app to run.
+    """
+    # BACKEND_BASE_URL
+    # ----------------
+    # This is just the name of THIS server.  We need it to build the links we put in
+    # confirmation emails.
+    hostname = os.environ.get("BACKEND_BASE_URL")
+    if (hostname is None):
+        logging.error("BACKEND_BASE_URL is missing -- exiting.")
+        exit(0)
+    logging.info("BACKEND_BASE_URL: " + hostname)
+
+    # INITIALIZE JWT LIB
+    # ------------------
+    # We're using a library (flask-jwt-extended) that handles JWT token management.
+    # It requires a key to be in the app context.  It should already be in the environment,
+    # just copy it over.
+    jwt_secret_key = os.environ.get("JWT_SECRET_KEY")
+    if not jwt_secret_key:
+        logging.error("JWT_SECRET_KEY is missing -- exiting.")
+        exit(0)
+    app.config['JWT_SECRET_KEY'] = jwt_secret_key
+    JWTManager(app)
+
+    # INITIALIZE CRYPTO LIB
+    # ---------------------
+    # We use a symmetric key to encrypt/decrypt sensitive data like the user's email address.
+    # Unlike the password, which we NEVER decrypt, we do need to decrypt these other values.
+    symmetric_key_b64 = os.environ.get("BACKEND_ENCRYPTION_KEY_B64")
+    if not symmetric_key_b64:
+        logging.error("BACKEND_ENCRYPTION_KEY_B64 is missing -- exiting.")
+        exit(0)
+    Crypto.initialize(symmetric_key_b64)
 
     # SET CORS POLICY
     # ---------------
+    # CORS (Cross-Origin Resource Policy) is an HTTP-header-based security mechanism 
+    # intended to stop man-in-the-middle attacks.  It allows a server to indicate any origins 
+    # (domain, scheme, or port) other than its own from which a browser should permit loading
+    # resources.  It's needed any time an app running in a browser at one domain accesses 
+    # resources or API endpoints running in a different domain. 
+    # 
+    # Crucially, CORS policy is enforced by the browser, not the app server, so you can't
+    # really disable it.  You *can* configure it to be maximally permissive, but obviously
+    # that's not great.
+    # 
     # We're using a library (flask-cors) that handles CORS policy for us.  
-    # Initialize it here.
-
+    #
     # In production, we'll need to account for port 443, 80, and maybe maybe 8080.
     #CORS(app, origins=["http://localhost:80", "http://localhost:443", "http://localhost:8080",
     #                   "http://www.trackeats.com:80", "http://www.trackeats.com:443", "http://www.trackeats.com:8080"])
-
+    #
     # I haven't worked out the correct domain names yet, so for now just allow
     # EVERYTHING.  This isn't secure, obviously, but I'll take my chances!
     #TODO: fix this!
     CORS(app, expose_headers=["Location"])
 
-    return app
 
-app = create_app()
-
-
-# Test the database connection.  Just calling "db.engine.connect()" is insufficient 
-# because it only tests the network connection, it doesn't try to log on.  So we try 
-# to run an actual query -- once which doesn't actually do anything, but finishes
-# quickly.
-def verify_database_connection():
+def verify_database_connection(app: Flask):
+    """
+    Test the database connection.  Just calling "db.engine.connect()" is insufficient 
+    because it only tests the network connection, it doesn't try to log on.  So we try 
+    to run an actual query -- once which doesn't actually do anything, but finishes
+    quickly.
+    """
     try:
         with app.app_context():
             with db.engine.connect() as conn:
@@ -153,7 +172,13 @@ def verify_database_connection():
         return "Could not connect to database: " + repr(e)
     return None
 
-def initialize_database():
+
+def initialize_database(app: Flask):
+    """
+    This can be used to recreate the database schema from scratch.  It does so if the User
+    table is not detected in the database, which is taken to mean that the database has
+    not been created yet.
+    """
     try:
         with app.app_context():
             Data.init_db()
@@ -163,14 +188,17 @@ def initialize_database():
 
 
 if __name__ == "__main__":
-    errmsg = verify_database_connection()
+    app = minimal_app_config()
+    additional_app_config(app)
+
+    errmsg = verify_database_connection(app)
     if (errmsg):
         logging.error(errmsg)
         sys.exit(0)
 
     logging.info("Database connection verified.")
 
-    errmsg = initialize_database()
+    errmsg = initialize_database(app)
     if (errmsg):
         logging.error(errmsg)
         sys.exit(0)
