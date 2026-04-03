b88e6924d87ca6a46883915fc63f706b00a9a3dd
Paul Holmes <pwholmes151@gmail.com>
2026-04-03 15:00:16 -0400

    adjust wording to account for Daily Log feature

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -43,10 +43,9 @@ const HomePage: React.FC = () => {
                     </Typography>
                     <Typography component="div" sx={{ pl: 3.5, fontFamily: 'monospace' }}>Username: guest</Typography>
                     <Typography component="div" sx={{ pl: 3.5, mb: 1.5, fontFamily: 'monospace' }}>Password: Guest*123</Typography>
-                    <Typography variant="body1" sx={{ mb: 1 }}>
-                        Feel free to play around -- all the data (including user accounts) is reset to a snapshot regularly.
-                    </Typography>
-                    <Typography variant="body1">See the About TrackEats page for more info about the app's technical design.</Typography>
+                    <Typography variant="body1" sx={{ mb: 1 }}>Feel free to play around -- all the data (including user accounts) is reset to a snapshot regularly.</Typography>
+                    <Typography variant="body1" sx={{ mb: 1 }}>See the About TrackEats page for more info about the app's technical design.</Typography>
+                    <Typography variant="body1">Note that TrackEats is NOT intended to be a mobile app.  It incorporates some reactive design techniques and will work well even if you shrink your browser window, but it will not look great on a phone.</Typography>
                 </Paper>
 
                 <List sx={{ listStyleType: 'disc', pl: 3.5, mb: 1.5 }}>
@@ -55,13 +54,33 @@ const HomePage: React.FC = () => {
                     <ListItem sx={{ display: 'list-item', py: 0.5 }}><Typography component="span" sx={{ fontWeight: 700 }}>Plan Meals </Typography><Typography component="span">Create meal plans that fit your lifestyle and goals.</Typography></ListItem>
                 </List>
 
-                <Typography sx={{ mb: 1.5 }}>TrackEats has a two-tier system: Foods and Recipes.</Typography>
                 <Typography sx={{ mb: 1.5 }}>
-                    To get started, think of a meal you like to make, and on the Foods page enter all the ingredients that are used to make that meal, along with their nutritional data. This is basically the data on each item's nutrition label when you buy it at the store. You can more or less copy it directly.
+                    TrackEats has a three-tier system: Foods, Recipes, and Daily Log:
                 </Typography>
-                <Typography sx={{ mb: 1.5 }}>Then go to the Recipes page and create a new Recipe, using those Foods as its ingredients.</Typography>
+                
+                <Box component="ul" sx={{ pl: 3, m: 0 }}>
+                    <Typography component="li" sx={{ mb: 1.5 }}>
+                        To get started, think of a meal you like to make, and on the Foods page enter all the
+                        ingredients that are used to make that meal, along with their nutritional data. This is
+                        basically the data on each food's nutrition label when you buy it at the store.
+                        You can more or less copy it directly.
+                    </Typography>
+
+                    <Typography component="li" sx={{ mb: 1.5 }}>
+                        Then go to the Recipes page and create a new Recipe, using those Foods as its ingredients.
+                    </Typography>
+                    
+                    <Typography component="li" sx={{ mb: 1.5 }}>
+                        Lastly, go to the Daily Log page and add the Recipes you have eaten on a particular day.
+                    </Typography>
+                </Box>
+
+
                 <Typography sx={{ mb: 1.5 }}>
-                    Once that is all entered, you will be able to see the complete per-serving nutrition data for that meal on the Recipes page. If you have entered price data for the Foods, you will even be able to see the per-serving cost of each Recipe. It is interesting to see how little (and occasionally, how much!) meals actually cost to make yourself.
+                    You will then be able to see the complete nutrition data for that day.
+                    If you have entered price data for the Foods, you will even be able to see the per-serving 
+                    cost of each Recipe. It is interesting to see how little (and occasionally, how much!) 
+                    meals actually cost to make yourself.
                 </Typography>
                 <Typography sx={{ fontStyle: 'italic', textAlign: 'center', mt: 2 }}>Start your culinary journey with TrackEats today!</Typography>
             </HomeCard>
