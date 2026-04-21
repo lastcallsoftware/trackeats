3fe479801cc6d29f2410156ed3d568e7e719aa31
Paul Holmes <pwholmes151@gmail.com>
2026-04-21 00:16:21 -0400

    add: create proxy script to route traffic from Windows to WSL

--- mobile/bin/proxy.bat
+++ mobile/bin/proxy.bat
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Route traffic on native Windows localhost:5000 to WSL
+# Route traffic from any source on native Windows port 5000 to the IP address on WSL where the backend is running
 netsh interface portproxy add v4tov4 listenport=5000 listenaddress=0.0.0.0 connectport=5000 connectaddress=172.19.77.76
+# Poke a hole in Windows Defender (aka Windows Firewall) at port 5000 so we don't have to disable the whole thing
 netsh advfirewall firewall add rule name="WSL Port 5000" dir=in action=allow protocol=TCP localport=5000
