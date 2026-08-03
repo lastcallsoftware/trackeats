ee2111eab7048008111dbb7d97b5a2b01d83ad10
Paul Holmes <pwholmes151@gmail.com>
2026-08-02 22:56:35 -0400

    fiddling with domain and port configs

--- frontend/bin/drun.sh
+++ frontend/bin/drun.sh
@@ -4,7 +4,6 @@ sudo docker run -d \
     --name trackeats-frontend \
     -p 80:8080 \
     -p 443:8443 \
-    -p 5443:5443 \
     -v /etc/ssl/certs:/etc/ssl/certs \
     --add-host=host.docker.internal:host-gateway \
     lastcallsoftware/trackeats-frontend
